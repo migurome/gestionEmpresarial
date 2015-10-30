286ee42c9625c82a74bf7983fbb8b5e808b950c7
Rome <migurome@ucm.es>
2015-10-30 11:50:56 +0100

    Delete prueba.sh

--- prueba.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ls
-echo "Se trata de hacer un archivo que pruebe el mytar"
-echo "Suele ir bien con el comando 'source nombre del script'"
-echo "redirigir salida de pantalla a un archivo con 'echo ''hola'' > archivo.algo"
